2662ad43bf3cb1e2554c77cec38b75fbaa776122
Andrew Acton <andrew.acton.aa@gmail.com>
2023-06-27 16:11:23 -0600

    Drag Gesture

---
+++ Pinch/Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Andrew Acton on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
