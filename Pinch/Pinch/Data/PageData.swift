d29c8b02660c0d5f67231efbfb8ffdc3e49befcb
Andrew Acton <andrew.acton.aa@gmail.com>
2023-06-28 10:25:59 -0600

    Complete

---
+++ Pinch/Pinch/Data/PageData.swift
@@ -0,0 +1,13 @@
+//
+//  PageData.swift
+//  Pinch
+//
+//  Created by Andrew Acton on 6/28/23.
+//
+
+import Foundation
+
+let pagesData: [Page] = [
+    Page(id: 1, imageName: "magazine-front-cover"),
+    Page(id: 2, imageName: "magazine-back-cover")
+]
